f700ac52994e46b60677be793fa1d71e3b407eb1
Andrea <dauriaandrea99@gmail.com>
2020-12-01 17:24:56 +0100

    Change in Function and alert

--- app.js
+++ app.js
@@ -4,28 +4,21 @@ Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha 
 Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 */
 
-// Array vuoto per numeri random
+// Dichiarazione array vuoto numeri generati
 var numRandom = [];
-// for per mostrare 5 volte un numero casuale e registrarlo nell'array vuoto
-for (var i=0 ; i < 5; i++) {
-    var x = Math.floor((Math.random() * 100) + 1);
-    // Ripetere 5 volte l'allert
-    alert(" il numero è : " + x); 
-    numRandom.push(x);
-}
-
-/*
-setTimeout(function(){ 
-    alert("Ti Attendono Interminabili 30 secondi"); 
-}, 30000);
-console.log(numeriUtente);
-*/
-
-/*
-// Dichiarazione array vuoto per numeri utente
+// Dichiarazione array vuoto numeri utente
 var numeriUtente = [];
-// Richiesta  5 numeri all'utente
-for (var i=0 ; i < 5; i++) {
-    numeriUtente.push(Number(prompt("Inserisci 5 numeri")));
-}
-*/
+  
+for (var i = 0; i < 5; i++) {
+    var x = randomNumberFromTo(1, 900);
+    numRandom.push(x);
+ };
+// Avviso all'utente
+console.log(numRandom);   
+alert("I tuoi numeri sono : " + numRandom);
+
+
+// FUNZIONE NUMERI RANDOM
+  function randomNumberFromTo(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
