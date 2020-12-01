9185b995fe79b6db177a3048a69a0528cc4e3948
Andrea <dauriaandrea99@gmail.com>
2020-12-01 16:57:34 +0100

    Random numb display done

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+/*
+Un alert espone 5 numeri casuali diversi.
+Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+*/
+
+// Array vuoto per numeri random
+var numRandom = [];
+// for per mostrare 5 volte un numero casuale e registrarlo nell'array vuoto
+for (var i=0 ; i < 5; i++) {
+    var x = Math.floor((Math.random() * 100) + 1);
+    // Ripetere 5 volte l'allert
+    alert(" il numero è : " + x); 
+    numRandom.push(x);
+}
+
+/*
+setTimeout(function(){ 
+    alert("Ti Attendono Interminabili 30 secondi"); 
+}, 30000);
+console.log(numeriUtente);
+*/
+
+/*
 // Dichiarazione array vuoto per numeri utente
 var numeriUtente = [];
 // Richiesta  5 numeri all'utente
 for (var i=0 ; i < 5; i++) {
     numeriUtente.push(Number(prompt("Inserisci 5 numeri")));
 }
-
-console.log(numeriUtente);
+*/
