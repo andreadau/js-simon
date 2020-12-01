90dc01a941fe96051a47e86223580536b3e438a5
Andrea <dauriaandrea99@gmail.com>
2020-12-01 16:19:44 +0100

    Start Web Page

---
+++ app.js
@@ -0,0 +1,8 @@
+// Dichiarazione array vuoto per numeri utente
+var numeriUtente = [];
+// Richiesta  5 numeri all'utente
+for (var i=0 ; i < 5; i++) {
+    numeriUtente.push(Number(prompt("Inserisci 5 numeri")));
+}
+
+console.log(numeriUtente);
